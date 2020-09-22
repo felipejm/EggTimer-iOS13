0fbf91e85e9e6a16f6adf9175fff074d678cac15
u003610 <felipe.martins@xpi.com.br>
2020-09-22 10:14:52 -0300

    first commit

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    private let hardnessTime = ["Soft": 5,"Medium": 7,"Hard": 12,]
+    
+    private var timer : Timer? = nil
+    private var timerCount : Int = 0
+    
+    private func resetTimer(){
+        timerCount = 0
+        timer?.invalidate()
+    }
+    
+    private func createTimer(until: Int){
+        timerCount = until
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { (timer) in
+            if(self.timerCount == 0){
+                self.resetTimer()
+                print("Stop boiling")
+            }else{
+                print(self.timerCount)
+                self.timerCount -= Int(timer.timeInterval)
+            }
+        }
+    }
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        resetTimer()
+        guard let title = sender.currentTitle else { return }
+        guard let timeToCook = hardnessTime[title] else { return }
 
-
+        print(title)
+        createTimer(until: timeToCook)
+    }
+    
 }
